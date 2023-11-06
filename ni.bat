5ed3e1b71fdcc913f1f841af2fbff3a70a83a4c9
nhoehne <Nathan.Hoehne@NicheRMS.com>
2023-11-06 15:43:47 -0600

    Change ni to touch

--- ni.bat
+++
@@ -1,6 +0,0 @@
-@REM Create a new file from cmd 
-@REM Usage: "ni filename.extension" 
-@REM Notes: 
-@REM - Meant to mirror PowerShell's ni (New-Item) 
-@REM - Similar to Unix's 'touch' 
-type nul >> %1
---
+++ touch.bat
@@ -0,0 +1,5 @@
+@REM Create a new file from Command Prompt or PowerShell
+@REM Usage: "touch filename.extension" 
+@REM Notes: 
+@REM - Similar to Unix's 'touch' 
+type nul >> %1
